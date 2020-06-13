179d363a097f54017ce7df2a4a4bfa4d529b2215
Sohaib Tanveer <58627661+rajasohaib91@users.noreply.github.com>
2020-06-13 18:34:25 +0500

    updated display msg

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App from Sohaib Tanveer<code>src/App.js</code> and save to
-          reload.
+          React App from Sohaib<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
